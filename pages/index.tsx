0f3cfcc80fcba01dae7f1be7f3f66f3c3da4b128
Pradeep Suthar <sutharp777@gmail.com>
2022-08-02 00:12:18 +0530

    configured :100:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import BgTexture from './components/bg-tecture';
+
+const Header: React.FC = () => (
+  <Head>
+    <title>●° Pradeep </title>
+    <link rel="icon" href="/favicon.png" />
+  </Head>);
+
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>● • . Pradeep </title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+    <>
+      <Header />
+      <BgTexture>
+        <div className='w-100wh h-100vh'>
+          <h1 className='text-4xl'>
+            Pradeep
+            Pradeep
+            Pradeep
+          </h1>
+        </div>
+      </BgTexture>
+    </>
+  );
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        
-
-
-
-
-
-
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
 }
 
 export default Home
